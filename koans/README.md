3a6df89b4d58dcfe60e3d43d344c8ba5e8ad7673
Mike Doel <mike@mikedoel.com>
2020-02-29 08:33:36 -0500

    getting started

---
+++ koans/README.md
@@ -0,0 +1,5 @@
+= Browser Testing Koans
+
+The Browser Testing Koans walk you along the path to enlightenment in order to learn
+how to test web applications in an automated fashion using Ruby.
+This has been *heavily* inspired by the EdgeCase Ruby Koans.
---
+++ src/about_driving_browsers.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require File.expand_path(File.dirname(__FILE__) + '/edgecase')
+
+require 'selenium-webdriver'
+
+class AboutDrivingBrowsers < Edgecase::Koan
+
+  def test_selenium_can_drive_firefox
+      driver = Selenium::WebDriver.for :firefox
+      driver.quit
+  end
+
+  def test_selenium_can_drive_chrome
+    driver = Selenium::WebDriver.for :chrome
+    driver.quit
+  end
+
+  def test_selenium_can_drive_edge
+    driver = Selenium::WebDriver.for :edge
+    driver.quit
+  end
+end
+
